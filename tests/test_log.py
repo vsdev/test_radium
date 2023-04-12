ca60e85aab85d65d230d77cdae71aad4616a3faf
v.s <vsworkbase@gmail.com>
2023-04-12 20:03:08 +0300

    Test log

---
+++ tests/test_log.py
@@ -0,0 +1,14 @@
+import logging
+from pathlib import Path
+
+import log
+
+
+def test_get_project_root_dir():
+    path = Path(__file__).absolute().parent.parent
+    assert log.get_project_root_dir() == str(path)
+
+
+def test_logging():
+    log.init_logger()
+    assert logging.getLogger()
